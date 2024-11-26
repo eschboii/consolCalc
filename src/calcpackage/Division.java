b6429d2b3b09233a9bb255ec0c359479f9343a71
Emile Schlick <emile.schlick@gmail.com>
2024-11-26 16:00:13 +0100

    fixat div

--- src/calcpackage/Division.java
+++ src/calcpackage/Division.java
@@ -2,5 +2,12 @@ package calcpackage;
 
 public class Division {
 
-    public int div(int a, int b) {return a / b;}
+    public int div(int a, int b) {
+        if (b == 0) {
+            return 0;
+        }
+
+        return a / b;
+    }
 }
+

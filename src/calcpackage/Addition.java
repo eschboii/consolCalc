e2872d6a7837adacf2824204ed7117074ce88c52
Emile Schlick <emile.schlick@gmail.com>
2024-11-26 15:49:54 +0100

    Skapat division samt fixat switchen lide

--- src/calcpackage/Addition.java
+++ src/calcpackage/Addition.java
@@ -1,13 +1,6 @@
 package calcpackage;
 
 public class Addition {
-<<<<<<< HEAD
-=======
-    public Addition (){
-
-    }
-
->>>>>>> 4140b4089275b4ee539e800a437ae5040635fbbb
 
     public int add(int a, int b){
         return a+b;
